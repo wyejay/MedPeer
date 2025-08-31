57cfb255a557624e9319bb632583fd8e8587f99e
wyejay <ebukaogbodo081@gmail.com>
2025-08-31 23:09:09 +0100

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,11 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
 from flask_login import LoginManager
-from flask_migrate import Migrate
+from flask_mail import Mail
 from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate
+from sqlalchemy.orm import DeclarativeBase
 
-db = SQLAlchemy()
-mail = Mail()
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
 login_manager = LoginManager()
-migrate = Migrate()
+mail = Mail()
 csrf = CSRFProtect()
+migrate = Migrate()
