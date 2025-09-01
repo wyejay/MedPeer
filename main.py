50f8068fa48e539261c5b96acd29b3502028698c
wyejay <ebukaogbodo081@gmail.com>
2025-09-01 02:03:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,3 @@ app = create_app(os.environ.get('FLASK_ENV', 'default'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-from app import create_app, db
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()
-    print("✅ Tables created!")
