6be8e2e4046e33b9e9120b5be1e7212815e490bc
wyejay <ebukaogbodo081@gmail.com>
2025-09-01 09:30:37 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+import os
 
+class Config:
+    SECRET_KEY = os.environ.get("SESSION_SECRET", "dev-secret-key-change-in-production")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///site.db")
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # must be set on Render
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "default": DevelopmentConfig,
+}
